63bf2ef670daa5ed2ad5dae9b2814613b6559675
Joseph G <codehaiku.io@gmail.com>
2019-10-10 19:17:43 +0800

    Added flash messages

---
+++ src/FlashMessage/FlashMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Subway\FlashMessage;
+
+class FlashMessage {
+
+	protected $user_id = 0;
+
+	protected $unique_key = '';
+
+	public function __construct( $user_id, $unique_key ) {
+
+		$this->user_id    = $user_id;
+
+		$this->unique_key = $unique_key;
+
+		return $this;
+
+	}
+
+	/**
+	 * @param array $messages
+	 *
+	 * @return $this
+	 * @throws \Exception
+	 */
+	public function add( $messages = [] ) {
+
+		if ( empty ( $this->user_id ) || empty ( $this->unique_key ) ) {
+
+			throw new \Exception( 'Error: user id and unique key must not be empty' );
+
+		}
+
+		update_user_meta( $this->user_id, $this->unique_key, $messages );
+
+		return $this;
+	}
+
+	public function get() {
+
+		$message = get_user_meta( $this->user_id, $this->unique_key );
+
+		// Immediately delete the flash message after getting it.
+		delete_user_meta( $this->user_id, $this->unique_key );
+
+		return $message;
+
+	}
+
+}
