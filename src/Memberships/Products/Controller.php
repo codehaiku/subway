b98d95c9818521df524dde4dae21c822518445b7
Joseph G <codehaiku.io@gmail.com>
2019-10-12 04:07:23 +0800

    Added validator

--- src/Memberships/Products/Controller.php
+++ src/Memberships/Products/Controller.php
@@ -4,10 +4,88 @@ namespace Subway\Memberships\Products;
 
 use Subway\FlashMessage\FlashMessage;
 use Subway\Helpers\Helpers;
+use Subway\Validators\GUMP;
 
 class Controller extends Products {
 
+	public function add_action() {
+
+		$this->check_admin();
+
+		check_admin_referer( 'subway_product_add_action', 'subway_product_add_action' );
+
+		$title = filter_input( 0, 'title', 513 );
+		$desc  = filter_input( 0, 'description', 513 );
+		$sku   = filter_input( 0, 'sku', 513 );
+
+		$validator = new GUMP();
+
+		$validator->validation_rules( array(
+			'title'       => 'required|alpha_space|max_len,200',
+			'description' => 'required',
+			'sku'         => 'required|alpha_numeric|max_len,100',
+		) );
+
+		$validated = $validator->run( $_POST );
+
+		if ( $validated === false ) {
+
+			$flash = new FlashMessage( get_current_user_id(), 'product-add-submit-messages' );
+
+			$flash->add( [
+				'validation' => $validator->get_errors_array(),
+				'form_data'  => [
+					'title'       => $title,
+					'description' => $desc,
+					'sku'         => $sku
+				]
+			] );
+
+			wp_safe_redirect( wp_get_referer(), 302 );
+
+			exit;
+		}
+
+		try {
+			$product_id = $this->add( [
+				'name'        => $title,
+				'description' => $desc,
+				'sku'         => $sku
+			] );
+
+		} catch ( \Exception $e ) {
+
+			echo $e->getMessage();
+
+		}
+
+		$redirect_url = add_query_arg(
+			[
+				'page'    => 'subway-membership',
+				'edit'    => 'yes',
+				'product' => $product_id
+			],
+			admin_url( 'admin.php?section=product-pricing' )
+		);
+
+		$redirect_url = $redirect_url . '#section-product-pricing';
+
+		$flash = new FlashMessage( get_current_user_id(), 'product-edit-submit-messages' );
+
+		$flash->add( [
+			'type'    => 'success',
+			'message' => esc_html__( 'Successfully added new membership plan. Go ahead and configure the pricing and email settings.', 'subway' )
+		] );
+
+		wp_safe_redirect( $redirect_url, 302 );
+
+		exit;
+
+	}
+
 	public function edit_action() {
+
+		$this->check_admin();
 
 		check_admin_referer( 'subway_product_edit_action', 'subway_product_edit_action' );
 
@@ -39,7 +117,10 @@ class Controller extends Products {
 
 			do_action( 'subway_product_edit_saved' );
 
-			$flash->add( [ 'type' => 'success', 'message' => __('Membership Plan has been successfully updated.', 'subway') ] );
+			$flash->add( [
+				'type'    => 'success',
+				'message' => __( 'Membership Plan has been successfully updated.', 'subway' )
+			] );
 
 			wp_safe_redirect( $referrer );
 
@@ -68,13 +149,26 @@ class Controller extends Products {
 
 	}
 
+	private function check_admin() {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'You are not allowed to do this.', 'subway' ) );
+		}
+
+		return;
+	}
+
 	public function attach_hooks() {
+
 		$this->define_hooks();
+
 	}
 
 	private function define_hooks() {
 
 		add_action( 'admin_post_subway_product_edit_action', [ $this, 'edit_action' ] );
+
+		add_action( 'admin_post_subway_product_add_action', [ $this, 'add_action' ] );
 
 	}
 
