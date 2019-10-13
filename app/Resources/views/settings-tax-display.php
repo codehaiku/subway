d569f895146bdc72a12ea01069a1dc6d0fc7cf43
Joseph G <codehaiku.io@gmail.com>
2019-10-14 07:02:01 +0800

    Added tax rates

---
+++ app/Resources/views/settings-tax-display.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template for showing tax on listing price.
+ */
+?>
+
+<p class="howto">
+    <label>
+
+        <input
+            <?php checked('1',$subway_display_tax,true); ?>
+                name="subway_display_tax"
+                value="1" type="checkbox"
+        />
+	    <?php esc_html_e('Check to include the tax rate on listing price.', 'subway'); ?>
+    </label>
+
+</p>
