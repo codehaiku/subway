27e25c9a20a5ac458ed0a45f714660760cc1d05a
Joseph G <codehaiku.io@gmail.com>
2019-10-14 10:16:24 +0800

    new updates in membership checkout table.

--- app/Resources/views/checkout-table.php
+++ app/Resources/views/checkout-table.php
@@ -14,7 +14,9 @@
                 <strong>
 					<?php echo esc_html( $product->get_name() ); ?>
                 </strong>
-                <a href="#">(Change)</a>
+                <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>" title="<?php esc_attr_e('(Change)','subway'); ?>">
+	                <?php esc_html_e('(Change)','subway'); ?>
+                </a>
             </td>
             <td>
                 <strong>
@@ -52,34 +54,45 @@
 
     <!-- Gateway Badge-->
     <div class="checkout-container">
-        <div class="subway-checkout-place-order">
-            <button type="submit" class="sw-button subway-checkout-place-order-button">
-				<?php esc_html_e( 'Place Order', 'subway' ); ?>
-            </button>
-        </div><!--.subway-checkout-place-order-->
-
+        <br/>
         <div class="subway-checkout-gateway-banner">
             <p>
-				<?php esc_html_e( 'You will be redirected to PayPal website to complete the payment.', 'subway' ); ?>
+			    <?php esc_html_e( 'Payments will be fulfilled through PayPal website for a secure checkout experience.', 'subway' ); ?>
             </p>
             <p>
                 <img class="gateway-img" width="200"
                      src="https://www.paypalobjects.com/webstatic/mktg/logo/AM_mc_vs_dc_ae.jpg"/>
             </p>
         </div><!--.subway-checkout-gateway-banner-->
+
+        <div class="subway-checkout-place-order">
+            <button type="submit" class="sw-button subway-checkout-place-order-button">
+				<?php esc_html_e( 'Place Order', 'subway' ); ?>
+            </button>
+        </div><!--.subway-checkout-place-order-->
+
+
     </div>
     <!-- Gateway Badge End-->
 
 <?php else: ?>
+    <?php if ( is_user_logged_in() ): ?>
+        <div class="subway-alert subway-alert-info">
+            <p>
+            <?php esc_html_e('Cannot switch to a free membership. You have already an active membership plan.', 'subway'); ?>
+            </p>
+        </div>
+    <?php else: ?>
+        <!-- Gateway Badge-->
+        <div class="checkout-container">
+            <div class="subway-checkout-place-order">
+                <button type="submit" class="sw-button subway-checkout-place-order-button">
+					<?php esc_html_e( 'Free Membership', 'subway' ); ?>
+                </button>
+            </div><!--.subway-checkout-place-order-->
+        </div>
+        <!-- Gateway Badge End-->
+    <?php endif; ?>
 
-    <!-- Gateway Badge-->
-    <div class="checkout-container">
-        <div class="subway-checkout-place-order">
-            <button type="submit" class="sw-button subway-checkout-place-order-button">
-				<?php esc_html_e( 'Free Membership', 'subway' ); ?>
-            </button>
-        </div><!--.subway-checkout-place-order-->
-    </div>
-    <!-- Gateway Badge End-->
 
 <?php endif; ?>
