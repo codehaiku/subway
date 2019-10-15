ac4988504a24639a3c4e591b7d13745ff73f12ac
Joseph G <codehaiku.io@gmail.com>
2019-10-15 11:21:54 +0800

    added user profile edit.

--- src/Hooks/Hooks.php
+++ src/Hooks/Hooks.php
@@ -1,18 +1,40 @@
 <?php
+
 namespace Subway\Hooks;
 
+use Subway\Options\Options;
 use Subway\Payment\Payment;
 
 class Hooks {
 
 	public function confirm_order() {
+
 		global $wpdb;
+
 		$payment = new Payment( $wpdb );
+
 		$payment->confirm();
+
+	}
+
+	public function edit_profile_url( $link, $user ) {
+
+		$options = new Options();
+
+		$accounts_url = $options->get_accounts_page_url();
+
+		$link = add_query_arg( 'account-page', 'update-personal-information', $accounts_url );
+
+		return $link;
+
 	}
 
 	public function listen() {
-		add_action('wp', array( $this, 'confirm_order' ) );
+
+		add_action( 'wp', array( $this, 'confirm_order' ) );
+
+		add_filter( 'get_edit_user_link', array( $this, 'edit_profile_url' ), 10, 2 );
+
 	}
 
 }
