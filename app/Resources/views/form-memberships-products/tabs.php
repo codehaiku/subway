c84d6434271e5a2584759e35bcde5abb8f24cf06
Joseph G <codehaiku.io@gmail.com>
2019-10-23 10:39:21 +0800

    Added Product Tabs

---
+++ app/Resources/views/form-memberships-products/tabs.php
@@ -0,0 +1,23 @@
+<ul id="product-tabs">
+    <li>
+        <a class="<?php echo esc_attr( $product->get_is_active_tab( 'settings' ) ); ?>"
+           href="<?php echo esc_url_raw( $product->get_product_url_edit() ); ?>">
+            <span class="dashicons dashicons-admin-settings"></span>
+			<?php esc_html_e( 'Settings', 'subway' ); ?>
+        </a>
+    </li>
+    <li>
+        <a class="<?php echo esc_attr( $product->get_is_active_tab( 'membership-plans' ) ); ?>"
+           href="<?php echo esc_url_raw( $product->get_product_url_edit( 'membership-plans' ) ); ?>">
+            <span class="dashicons dashicons-buddicons-groups"></span>
+			<?php esc_html_e( 'Membership Plans', 'subway' ); ?>
+        </a>
+    </li>
+    <li>
+        <a class="<?php echo esc_attr( $product->get_is_active_tab( 'subscribers' ) ); ?>"
+           href="<?php echo esc_url_raw( $product->get_product_url_edit( 'subscribers' ) ); ?>">
+            <span class="dashicons dashicons-groups"></span>
+			<?php esc_html_e( 'Subscribers', 'subway' ); ?>
+        </a>
+    </li>
+</ul>
