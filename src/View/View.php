65a5ea1e78bcd2e6ddd44cb1c364169d97032e0c
Joseph G <codehaiku.io@gmail.com>
2019-10-23 10:39:21 +0800

    Added Product Tabs

--- src/View/View.php
+++ src/View/View.php
@@ -4,16 +4,24 @@ namespace Subway\View;
 
 class View {
 
-	public function render( $file_location, $args, $return = false ) {
+	public function render( $file_location, $args, $return = false, $sub_dir = '' ) {
 
 		extract( $args );
 
-		$file_path = SUBWAY_DIR_PATH . 'app/Resources/views/' . sanitize_title( $file_location ) . '.php';
+		$file = sanitize_file_name( $file_location );
+
+		$sub_dir = sanitize_file_name( $sub_dir );
+
+		if ( ! empty( $sub_dir ) ) {
+			$sub_dir = sprintf( '%s/', $sub_dir );
+		}
+
+		$file_path = SUBWAY_DIR_PATH . 'app/Resources/views/' . $sub_dir . $file . '.php';
 
 		if ( ! file_exists( $file_path ) ) {
 
 			echo "<code>";
-			echo sprintf( __( 'ERROR: Cannot find %s in view directory', 'subway' ), sanitize_title( $file_location ) );
+				echo sprintf( __( 'ERROR: Cannot find %s %s in view directory', 'subway' ), $sub_dir, $file );
 			echo "</code>";
 
 			return $this;
