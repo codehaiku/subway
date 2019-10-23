e56286714dc573586e081806a3e470ac6ed80ed3
Joseph G <codehaiku.io@gmail.com>
2019-10-23 16:09:51 +0800

    Refactor membership products to plans

--- app/Resources/views/form-memberships-products-edit.php
+++ app/Resources/views/form-memberships-products-edit.php
@@ -1,5 +1,7 @@
 <div class="wrap">
+
 	<?php
+
 	// The product.
 	$product_id = filter_input( 1, 'id', 516 );
 
@@ -7,7 +9,7 @@
 	$product->set_id( $product_id );
 	$product->get();
 
-	// Error Messages
+	// Error Messages.
 	$flash   = new \Subway\FlashMessage\FlashMessage( get_current_user_id(), 'subway_product_edit' );
 	$notices = $flash->get();
 
@@ -16,22 +18,25 @@
 		'notices' => $notices,
 		'product' => $product
 	];
+
 	?>
 
     <h1 class="wp-heading-inline">
 
-		<?php echo sprintf( esc_html__( 'Configure %s', 'subway' ), $product->get_name() ); ?>
+		<?php echo esc_html( $product->get_name() ); ?>
 
     </h1>
 
-    <hr class="wp-header-end">
+    <hr class="wps-header-end">
 
 	<?php if ( isset( $notices['type'] ) && $notices['type'] === 'success' ): ?>
 
         <div class="notice notice-success is-dismissible">
+
             <p>
 				<?php echo esc_html( $notices['message'] ); ?>
             </p>
+
         </div>
 
 	<?php endif; ?>
@@ -41,6 +46,7 @@
         <div class="subway-flex-wrap">
 
             <div class="subway-flex-column-70">
+
                 <!-- Product Tabs -->
 				<?php $this->render( 'tabs', $extracted_vars, false, 'form-memberships-products' ); ?>
                 <!-- Product Tabs End -->
@@ -48,6 +54,7 @@
                 <div>
                     <!--Hidden Fields-->
                     <input type="hidden" name="action" value="subway_product_edit"/>
+
                     <input type="hidden" name="id" value="<?php echo esc_attr( $product_id ); ?>"/>
                     <!--Hidden Fields End-->
                 </div>
@@ -86,13 +93,17 @@
 
 
             <div class="subway-flex-column-30">
+
                 <div class="subway-flex-inner-wrap" style="margin-top:3.3em">
+
 					<?php $this->render( 'status', $extracted_vars, false, 'form-memberships-products' ); ?>
+
                 </div>
+
             </div>
 
         </div><!--FlexWrap-->
+
     </form>
 
-
-</div>
+</div><!--.wrap-->
