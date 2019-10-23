2e8fb54128eb119754fd372eb35901c9914ffc9b
Joseph G <codehaiku.io@gmail.com>
2019-10-23 10:39:21 +0800

    Added Product Tabs

--- app/Resources/views/form-memberships-products-edit.php
+++ app/Resources/views/form-memberships-products-edit.php
@@ -10,10 +10,18 @@
 	// Error Messages
 	$flash   = new \Subway\FlashMessage\FlashMessage( get_current_user_id(), 'subway_product_edit' );
 	$notices = $flash->get();
+
+	// Extract the variables to our partial views.
+	$extracted_vars = [
+		'notices' => $notices,
+		'product' => $product
+	];
 	?>
 
     <h1 class="wp-heading-inline">
+
 		<?php echo sprintf( esc_html__( 'Configure %s', 'subway' ), $product->get_name() ); ?>
+
     </h1>
 
     <hr class="wp-header-end">
@@ -25,6 +33,7 @@
 				<?php echo esc_html( $notices['message'] ); ?>
             </p>
         </div>
+
 	<?php endif; ?>
 
     <form autocomplete="off" method="post" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
@@ -32,213 +41,54 @@
         <div class="subway-flex-wrap">
 
             <div class="subway-flex-column-70">
-
-                <ul id="product-tabs">
-                    <li>
-                        <a class="active" data-section-target="product-information" href="#">
-                            <span class="dashicons dashicons-admin-settings"></span>
-							<?php esc_html_e( 'Settings', 'subway' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-section-target="product-information" href="#">
-                            <span class="dashicons dashicons-buddicons-groups"></span>
-							<?php esc_html_e( 'Membership Plans', 'subway' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-section-target="product-information" href="#">
-                            <span class="dashicons dashicons-groups"></span>
-							<?php esc_html_e( 'Subscribers', 'subway' ); ?>
-                        </a>
-                    </li>
-
-                </ul>
-
+                <!-- Product Tabs -->
+				<?php $this->render( 'tabs', $extracted_vars, false, 'form-memberships-products' ); ?>
+                <!-- Product Tabs End -->
 
                 <div>
-                    <!--hidden fields-->
+                    <!--Hidden Fields-->
                     <input type="hidden" name="action" value="subway_product_edit"/>
                     <input type="hidden" name="id" value="<?php echo esc_attr( $product_id ); ?>"/>
+                    <!--Hidden Fields End-->
                 </div>
 
-                <div class="subway-card subway-product-section active">
+                <div class="subway-card subway-tab-section subway-product-section <?php echo esc_attr( $product->get_is_active_tab( 'settings' ) ); ?>">
 
-                    <!--Product Name -->
-                    <div class="subway-form-row">
-                        <h3 class="field-title">
-                            <label for="input-title">
-								<?php esc_html_e( 'Name', 'subway' ); ?>
-                            </label>
-                        </h3>
-                        <p>
-							<?php esc_html_e( 'Enter the name of your membership product', 'subway' ); ?>
-                        </p>
-                        <input autofocus value="<?php echo esc_attr( $product->get_name() ); ?>" id="input-title"
-                               name="name" type="text" class="widefat"
-                               placeholder="<?php esc_attr_e( 'New product name', 'subway' ); ?>"/>
+	                <?php if ( 'active' === $product->get_is_active_tab( 'settings' ) ): ?>
 
-						<?php if ( isset( $notices['message']['name'] ) ): ?>
-                            <p class="validation-errors">
-								<?php echo $notices['message']['name']; ?>
-                            </p>
-						<?php endif; ?>
-                    </div>
-                    <!--/.Product Name -->
+					    <?php $this->render( 'settings', $extracted_vars, false, 'form-memberships-products' ); ?>
 
-
-                    <!-- Product Description -->
-                    <div class="subway-form-row">
-                        <h3 class="field-title">
-                            <label for="input-description">
-								<?php esc_html_e( 'Description', 'subway' ); ?>
-                            </label>
-                        </h3>
-                        <p>
-							<?php esc_html_e( 'Explain what this product is all about.', 'subway' ); ?>
-                        </p>
-                        <textarea id="input-description" name="description" class="widefat" rows="5"
-                                  placeholder="<?php esc_attr_e( 'New product description', 'subway' ); ?>"><?php echo wp_kses_post( $product->get_description() ); ?></textarea>
-						<?php if ( isset( $notices['message']['description'] ) ): ?>
-                            <p class="validation-errors">
-								<?php echo $notices['message']['description']; ?>
-                            </p>
-						<?php endif; ?>
-                    </div>
-                    <!--/. Product Description -->
-
-                    <!--Tax Rate -->
-                    <div class="subway-form-row">
-                        <h3 class="field-title">
-                            <label for="input-tax-rate">
-								<?php esc_html_e( 'Tax Rate', 'subway' ); ?>
-                            </label>
-                        </h3>
-                        <p>
-							<?php esc_html_e( 'Enter the tax rate as percentage for this product.', 'subway' ); ?>
-                        </p>
-                        <input step="0.01" value="<?php echo esc_attr( $product->get_tax_rate() ); ?>"
-                               id="input-tax-rate"
-                               name="tax_rate" type="number" class="widefat"
-                               placeholder="<?php esc_attr_e( 'Enter 12 for 12%. Enter 0 or 0.00 to disable.', 'subway' ); ?>"/>
-						<?php if ( isset( $notices['message']['tax_rate'] ) ): ?>
-                            <p class="validation-errors">
-								<?php echo $notices['message']['tax_rate']; ?>
-                            </p>
-						<?php endif; ?>
-                    </div>
-                    <!--/.Tax Rate -->
-
-                    <!--Tax Rate Display-->
-                    <div class="subway-form-row">
-                        <h3 class="field-title">
-                            <label for="input-tax-rate-display">
-								<?php esc_html_e( 'Display Tax', 'subway' ); ?>
-                            </label>
-                        </h3>
-                        <p>
-                            <label for="input-tax-rate-display">
-                                <input <?php checked( true, $product->is_tax_displayed(), true ); ?>
-                                        type="checkbox" name="tax_rate_displayed" id="input-tax-rate-display"/>
-								<?php esc_html_e( 'Check to include tax in membership plans pricing.', 'subway' ); ?>
-                            </label>
-                        </p>
-                    </div>
-                    <!--/.Tax Rate -->
-
-                    <!--Prorating-->
-                    <div class="subway-form-row">
-                        <h3 class="field-title">
-                            <label for="is_prorated">
-								<?php esc_html_e( 'Prorate Charges', 'subway' ); ?>
-                            </label>
-                        </h3>
-                        <p>
-                            <label for="is_prorated">
-                                <input checked type="checkbox" name="is_prorated" id="is_prorated"/>
-								<?php esc_html_e( 'Check to automatically prorate charges when member switch to any plan of this product.', 'subway' ); ?>
-                            </label>
-                        </p>
-                    </div>
-                    <!--/.Tax Rate -->
+                    <?php endif; ?>
 
                 </div>
 
+                <div class="subway-card subway-tab-section subway-membership-plans-section <?php echo esc_attr( $product->get_is_active_tab( 'membership-plans' ) ); ?>">
+
+                    <?php if ( 'active' === $product->get_is_active_tab( 'membership-plans' ) ): ?>
+
+						<?php $this->render( 'plans', $extracted_vars, false, 'form-memberships-products' ); ?>
+
+                    <?php endif; ?>
+
+                </div>
+
+                <div class="subway-card subway-tab-section subway-subscribers-section <?php echo esc_attr( $product->get_is_active_tab( 'subscribers' ) ); ?>">
+
+                    <?php if ( 'active' === $product->get_is_active_tab( 'subscribers' ) ): ?>
+
+						<?php $this->render( 'users', $extracted_vars, false, 'form-memberships-products' ); ?>
+
+                    <?php endif; ?>
+
+                </div>
 
             </div>
 
 
             <div class="subway-flex-column-30">
                 <div class="subway-flex-inner-wrap" style="margin-top:3.3em">
-                    <div class="subway-card">
-                        <!--Product Status-->
-                        <div class="subway-form-row">
-                            <h3 class="field-title">
-                                <label for="product-status">
-									<?php esc_html_e( 'Product Status', 'subway' ); ?>
-                                </label>
-                            </h3>
-                            <p class="field-help">
-								<?php esc_html_e( 'Change the value of this field to \'Published\' to make this product available to
-                                    everyone.', 'subway' ); ?>
-                            </p>
-							<?php
-							$statuses = [
-								'draft'     => __( 'Draft', 'subway' ),
-								'published' => __( 'Published', 'subway' )
-							];
-							?>
-
-                            <select id="product-status" name="status">
-								<?php foreach ( $statuses as $key => $value ): ?>
-									<?php if ( $key === $product->get_status() ): ?>
-										<?php $selected = 'selected'; ?>
-									<?php else: ?>
-										<?php $selected = ''; ?>
-									<?php endif; ?>
-                                    <option value="<?php echo esc_attr( $key ); ?>" <?php echo esc_attr( $selected ); ?>>
-										<?php echo esc_html( $value ); ?>
-                                    </option>
-								<?php endforeach; ?>
-                            </select>
-                        </div>
-                        <!--/.Product Status-->
-                        <!--Product Link -->
-                        <div class="subway-form-row" id="checkout-link-section">
-                            <h3 class="field-title">
-                                <label for="input-checkout-link">
-									<?php esc_html_e( 'Copy Product URL', 'subway' ); ?>
-                                </label>
-                            </h3>
-                            <p class="field-help">
-								<?php esc_html_e( 'Use the url below to share this membership product', 'subway' ); ?>
-                            </p>
-                            <input readonly="" value="http://multisite.local/checkout?product_id=2"
-                                   id="input-checkout-link" type="url">
-                            <p>
-                                <button id="btn-copy-checkout-link" type="button"
-                                        class="button button-primary button-small">
-                                    Copy Link
-                                </button>
-                                <a href="http://multisite.local/checkout?product_id=2" target="_blank"
-                                   class="button button-secondary button-small">
-                                    Visit Link </a>
-                            </p>
-                        </div>
-                        <!--.Product Link -->
-
-
-                    </div>
-                    <div class="subway-card">
-                        <div>
-                            <input id="publish-product" type="submit" class="button button-primary button-large"
-                                   value="<?php esc_attr_e( 'Update Product', 'subway' ); ?>">
-                        </div>
-                    </div>
-
+					<?php $this->render( 'status', $extracted_vars, false, 'form-memberships-products' ); ?>
                 </div>
-
-
             </div>
 
         </div><!--FlexWrap-->
