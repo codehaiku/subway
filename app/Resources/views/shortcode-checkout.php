bc7b7b30c98cd4a922e1b24b7c21f371196b4f72
Joseph G <codehaiku.io@gmail.com>
2019-10-24 23:20:16 +0800

    new updates

--- app/Resources/views/shortcode-checkout.php
+++ app/Resources/views/shortcode-checkout.php
@@ -1,103 +1,125 @@
-<?php
-$product_id = filter_input( INPUT_GET, 'product_id', 519 );
-$user       = wp_get_current_user();
-?>
-<div id="subway-checkout-wrap">
+<?php if ( $plan ): ?>
 
-    <div id="subway-checkout-inner-wrap">
+	<?php $plan_id = $plan->get_id(); ?>
 
-        <form class="sw-form" action="" method="POST">
+	<?php $user = wp_get_current_user(); ?>
 
-            <input type="hidden" name="sw-action" value="checkout"/>
+    <div id="subway-checkout-wrap">
 
-            <input type="hidden" name="sw-product-id" value="<?php echo absint( $product_id ); ?>"/>
+        <div id="subway-checkout-inner-wrap">
 
-            <div class="subway-checkout-user-info">
-                <div class="subway-checkout-user-info-title">
-                    <h3>
-						<?php esc_html_e( 'Review Order', 'subway' ); ?>
-                    </h3>
-                </div>
+            <form class="sw-form" action="" method="POST">
 
-                <div class="subway-checkout-user-info-table-wrap">
-                    <table class="subway-checkout-user-info-table">
-                        <thead>
-                        <tr>
-                            <th colspan="2">
-								<?php esc_html_e( 'Personal Information', 'subway' ); ?>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>
-								<?php esc_html_e( 'Name', 'subway' ); ?>
-                            </td>
-                            <td>
-                                <div class="subway-flex-wrap">
-                                    <div class="subway-flex-column-20">
-										<?php echo get_avatar( get_current_user_id(), 40 ); ?><br/>
+                <input type="hidden" name="sw-action" value="checkout"/>
+
+                <input type="hidden" name="sw-product-id" value="<?php echo absint( $plan_id ); ?>"/>
+
+                <div class="subway-checkout-user-info">
+                    <div class="subway-checkout-user-info-title">
+                        <h3>
+							<?php esc_html_e( 'Review Order', 'subway' ); ?>
+                        </h3>
+                    </div>
+
+                    <div class="subway-checkout-user-info-table-wrap">
+                        <table class="subway-checkout-user-info-table">
+                            <thead>
+                            <tr>
+                                <th colspan="2">
+									<?php esc_html_e( 'Personal Information', 'subway' ); ?>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+									<?php esc_html_e( 'Name', 'subway' ); ?>
+                                </td>
+                                <td>
+                                    <div class="subway-flex-wrap">
+                                        <div class="subway-flex-column-20">
+											<?php echo get_avatar( get_current_user_id(), 40 ); ?><br/>
+                                        </div>
+                                        <div class="subway-flex-column-80">
+
+											<?php echo esc_html( $user->display_name ); ?>
+                                            <br/>
+                                            <a title="<?php esc_attr_e( '(Not You?) Logout', 'subway' ); ?>"
+                                               href="<?php echo esc_url( wp_logout_url() ); ?>">
+												<?php esc_html_e( '(Not You?) Logout', 'subway' ); ?>
+                                            </a>
+
+                                        </div>
                                     </div>
-                                    <div class="subway-flex-column-80">
-
-										<?php echo esc_html( $user->display_name ); ?>
-                                        <br/>
-                                        <a title="<?php esc_attr_e( '(Not You?) Logout', 'subway' ); ?>"
-                                           href="<?php echo esc_url( wp_logout_url() ); ?>">
-											<?php esc_html_e( '(Not You?) Logout', 'subway' ); ?>
-                                        </a>
-
-                                    </div>
-                                </div>
 
 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-								<?php esc_html_e( 'Email Address', 'subway' ); ?>
-                            </td>
-                            <td>
-								<?php echo esc_html( $user->user_email ); ?>
-                                <br/>
-                                <a href="<?php echo esc_url( add_query_arg( 'edit', 'profile', $options->get_accounts_page_url() ) ); ?>"
-                                   title="<?php esc_attr_e( 'Update Email Address', 'subway' ); ?>">
-									<?php esc_html_e( 'Update Email Address', 'subway' ); ?>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <?php esc_html_e('Current Memberships Plan', 'subway'); ?>
-                            </td>
-                            <td>
-                                <strong>
-                                    <a title="<?php echo esc_attr( $product->get_name() ); ?>" href="<?php echo esc_url( $product->get_plan_url() ); ?>">
-                                        <?php echo esc_html( $product->get_name() ); ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+									<?php esc_html_e( 'Email Address', 'subway' ); ?>
+                                </td>
+                                <td>
+									<?php echo esc_html( $user->user_email ); ?>
+                                    <br/>
+                                    <a href="<?php echo esc_url( add_query_arg( 'edit', 'profile', $options->get_accounts_page_url() ) ); ?>"
+                                       title="<?php esc_attr_e( 'Update Email Address', 'subway' ); ?>">
+										<?php esc_html_e( 'Update Email Address', 'subway' ); ?>
                                     </a>
-                                </strong>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+									<?php esc_html_e( 'Current Memberships Plan', 'subway' ); ?>
+                                </td>
+                                <td>
+                                    <strong>
+                                        <a title="<?php echo esc_attr( $plan->get_name() ); ?>"
+                                           href="<?php echo esc_url( $plan->get_plan_url() ); ?>">
+											<?php echo esc_html( $plan->get_name() ); ?>
+                                        </a>
+                                    </strong>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
 
-                </div>
-            </div><!--.subway-checkout-user-info-->
+                    </div>
+                </div><!--.subway-checkout-user-info-->
 
-            <div class="subway-checkout-review-order-wrap">
+                <div class="subway-checkout-review-order-wrap">
 
-				<?php $view->render( 'checkout-table', [ 'product' => $product, 'currency' => $currency, 'options' => $options ] ); ?>
+					<?php
+					$view->render(
+						'checkout-table',
+						[
+							'plan'     => $plan,
+							'currency' => $currency,
+							'options'  => $options
+						] );
+					?>
 
-            </div><!--.subway-checkout-review-order-->
+                </div><!--.subway-checkout-review-order-->
 
-        </form>
+            </form>
 
-    </div><!--.subway-checkout-inner-wrap-->
+        </div><!--.subway-checkout-inner-wrap-->
 
-</div><!--.subway-checkout-wrap-->
+    </div><!--.subway-checkout-wrap-->
 
-<script>
-    if (window.history.replaceState) {
-        window.history.replaceState(null, null, window.location.href);
-    }
-</script>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
+<?php else: ?>
+    <div class="subway-alert subway-alert-info">
+        <p>
+			<?php esc_html_e( 'Error: Plan not found', 'subway' ); ?>
+            <br/>
+            <a class="sw-button" href="<?php echo esc_url(  $options->get_membership_page_url()  ); ?>" title="<?php esc_attr_e('Select Membership','subway'); ?>">
+                <?php esc_html_e('Select Membership Plan','subway'); ?>
+            </a>
+        </p>
+    </div>
+<?php endif; ?>
