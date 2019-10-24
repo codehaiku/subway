7a7cc3198be217c155d590a6ebc321111af64afd
Joseph G <codehaiku.io@gmail.com>
2019-10-25 02:13:33 +0800

    Refactor register to change products into plans

--- app/Resources/views/form-memberships-products/plans.php
+++ app/Resources/views/form-memberships-products/plans.php
@@ -3,21 +3,20 @@ $plans = new \Subway\Memberships\Plan\Plan();
 $items = $plans->get_plans( [ 'product_id' => $product->get_id() ] );
 ?>
 
-<p>
-    <a href="<?php echo esc_url_raw( $plans->get_add_url( $product->get_id() ) ); ?>" class="button button-large button-primary">
-        <span>&plus;</span>
-		<?php esc_html_e('Create New Plan', 'subway'); ?>
-    </a>
-</p>
-
 <?php if ( ! empty( $items ) ): ?>
     <table class="wp-list-table widefat fixed striped" id="boxmembership-product-plans-table">
         <thead>
         <tr>
-            <th colspan="5">
-                <strong>
-                    <?php esc_html_e('Membership Plans','subway'); ?>
-                </strong>
+            <th colspan="1">
+				<?php esc_html_e( 'Membership Plans', 'subway' ); ?>
+
+            </th>
+            <th colspan="4">
+                <a href="<?php echo esc_url_raw( $plans->get_add_url( $product->get_id() ) ); ?>"
+                   class="button button-small button-secondary">
+                    <span>&plus;</span>
+					<?php esc_html_e( 'Create New Plan', 'subway' ); ?>
+                </a>
             </th>
         </tr>
         <tr>
@@ -37,39 +36,43 @@ $items = $plans->get_plans( [ 'product_id' => $product->get_id() ] );
 							<?php echo esc_html( $plan->get_name() ); ?>
                         </strong>
                     </a>
+
                     <div class="row-actions">
-                        <a href="<?php echo esc_url_raw( $plan->get_edit_url( $plan->get_id(), $product->get_id() ) ); ?>" class="">
-                            <?php esc_html_e('Edit', 'subway'); ?>
+                        <a href="<?php echo esc_url_raw( $plan->get_edit_url( $plan->get_id(), $product->get_id() ) ); ?>"
+                           class="">
+							<?php esc_html_e( 'Edit', 'subway' ); ?>
                         </a>
                         |
-                        <a href="<?php echo esc_url_raw( $plan->get_edit_url( $plan->get_id(), $product->get_id() ) ); ?>" class="">
-	                        <?php esc_html_e('View', 'subway'); ?>
+                        <a href="<?php echo esc_url_raw( $plan->get_edit_url( $plan->get_id(), $product->get_id() ) ); ?>"
+                           class="">
+							<?php esc_html_e( 'View', 'subway' ); ?>
                         </a>
                     </div>
                 </td>
 
                 <td>
 					<?php echo esc_html( $plan->get_displayed_price() ); ?>
+
                 </td>
 
                 <td>
-					<?php echo esc_html( ucfirst( $plan->get_type() ) ); ?>
+	                    <?php echo esc_html( ucfirst( $plan->get_type() ) ); ?>
                 </td>
 
                 <td>
-					<?php echo esc_html( ucfirst($plan->get_status() )); ?>
+					<?php echo esc_html( ucfirst( $plan->get_status() ) ); ?>
                 </td>
 
                 <td>
-					<?php echo esc_html( $plan->get_date_updated() ); ?>
+					<?php echo esc_html( date('M d, o h:s A', strtotime( $plan->get_date_updated() ) ) ); ?>
                 </td>
             </tr>
 		<?php endforeach; ?>
         </tbody>
         <tfoot>
         <tr>
-            <td colspan="5">
-               <?php echo sprintf( esc_html__('Found %d Item(s)', 'subway'), count( $items ) ); ?>
+            <td colspan="4">
+				<?php echo sprintf( esc_html__( 'Found %d Item(s)', 'subway' ), count( $items ) ); ?>
             </td>
         </tr>
         </tfoot>
