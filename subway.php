8fc8f4b2751039bf5678dbbe43344fbf14f1f002
Joseph G <codehaiku.io@gmail.com>
2019-10-25 18:40:54 +0800

    new updates

--- subway.php
+++ subway.php
@@ -24,13 +24,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
 
-global $wpdb;
-
 // Define Subway Plugin Version.
 define( 'SUBWAY_VERSION', '3.0' );
 
 // Define Database Version.
-define( 'SUBWAY_DB_VERSION', '1.0.5' );
+define( 'SUBWAY_DB_VERSION', '1.0.6' );
 
 // Define Subway Directory Path.
 define( 'SUBWAY_DIR_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
