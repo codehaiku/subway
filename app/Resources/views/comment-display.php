43ccc292325bc1246c0da06f2d8f09dd06f8bbbb
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:05 +0800

    new update

--- app/Resources/views/comment-display.php
+++ app/Resources/views/comment-display.php
@@ -1,5 +1,5 @@
 <div class="subway-comment-closed">
 	<?php
-		echo wp_kses_post( get_option('subway_comment_limited_message',esc_html__('Commenting is limited.','subway') ) );
+	echo wp_kses_post( get_option( 'subway_comment_limited_message', esc_html__( 'Commenting is limited.', 'subway' ) ) );
 	?>
 </div>
