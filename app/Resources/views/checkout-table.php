b851d20f65b17e31fcde590d64d6a99d693ab3da
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:05 +0800

    new update

--- app/Resources/views/checkout-table.php
+++ app/Resources/views/checkout-table.php
@@ -14,8 +14,9 @@
                 <strong>
 					<?php echo esc_html( $plan->get_name() ); ?>
                 </strong>
-                <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>" title="<?php esc_attr_e('(Change)','subway'); ?>">
-	                <?php esc_html_e('(Change)','subway'); ?>
+                <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>"
+                   title="<?php esc_attr_e( '(Change)', 'subway' ); ?>">
+					<?php esc_html_e( '(Change)', 'subway' ); ?>
                 </a>
             </td>
             <td>
@@ -57,7 +58,7 @@
         <br/>
         <div class="subway-checkout-gateway-banner">
             <p>
-			    <?php esc_html_e( 'Payments will be fulfilled through PayPal website for a secure checkout experience.', 'subway' ); ?>
+				<?php esc_html_e( 'Payments will be fulfilled through PayPal website for a secure checkout experience.', 'subway' ); ?>
             </p>
             <p>
                 <img class="gateway-img" width="200"
@@ -76,19 +77,19 @@
     <!-- Gateway Badge End-->
 
 <?php else: ?>
-    <?php if ( is_user_logged_in() ): ?>
+	<?php if ( is_user_logged_in() ): ?>
         <div class="subway-alert subway-alert-info">
             <p>
-            <?php esc_html_e('Cannot switch to a free membership. You have already an active membership plan.', 'subway'); ?>
+				<?php esc_html_e( 'Cannot switch to a free membership. You have already an active membership plan.', 'subway' ); ?>
             </p>
         </div>
-    <?php else: ?>
+	<?php else: ?>
         <!-- Gateway Badge-->
         <div class="checkout-container">
             <div class="subway-checkout-place-order">
                 <p>
-	                <?php esc_html_e( 'This membership plan is free.', 'subway' ); ?><br/>
-	                <?php esc_html_e( ' Click the button below to proceed.', 'subway' ); ?>
+					<?php esc_html_e( 'This membership plan is free.', 'subway' ); ?><br/>
+					<?php esc_html_e( ' Click the button below to proceed.', 'subway' ); ?>
                 </p>
                 <button type="submit" class="sw-button subway-checkout-place-order-button">
 					<?php esc_html_e( 'Free Memberships', 'subway' ); ?>
@@ -96,7 +97,7 @@
             </div><!--.subway-checkout-place-order-->
         </div>
         <!-- Gateway Badge End-->
-    <?php endif; ?>
+	<?php endif; ?>
 
 
 <?php endif; ?>
