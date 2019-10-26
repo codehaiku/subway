28523b8f8e2cdc4953a4444fba51fb057da39098
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:07 +0800

    new update

--- src/Post/Comments.php
+++ src/Post/Comments.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Subway\Post;
 
 use Subway\User\User;
@@ -6,116 +7,100 @@ use Subway\View\View;
 
 class Comments {
 
-    protected $user;
+	protected $user;
 
-    protected $view;
+	protected $view;
 
-    public function __construct( User $user, View $view)
-    {
-        $this->user = $user;
-        $this->view = $view;
-    }
+	public function __construct( User $user, View $view ) {
+		$this->user = $user;
+		$this->view = $view;
+	}
 
-    public function is_current_user_allowed()
-    {
-        
-        $post_id = get_the_id();
+	public function is_current_user_allowed() {
 
-        $access_type = get_post_meta( $post_id, 'subway_post_discussion_access_type', true );
+		$post_id = get_the_id();
 
-        // Allow commenting when editor unchecked the 'Limit to roles or membership types'.
-        if ( empty ( $access_type ) )
-        {
-            return apply_filters('subway_post_discussion_allow_comment', true);
-        }
+		$access_type = get_post_meta( $post_id, 'subway_post_discussion_access_type', true );
 
-        // If access type is not empty. Editor limits the commenting.
-        if ( ! empty ( $access_type ) )
-        {
-            // Allow administrator.
-            if ( current_user_can( 'manage_option' ) ) 
-            {
-                return apply_filters('subway_post_discussion_allow_comment', true);
-            }
+		// Allow commenting when editor unchecked the 'Limit to roles or membership types'.
+		if ( empty ( $access_type ) ) {
+			return apply_filters( 'subway_post_discussion_allow_comment', true );
+		}
 
-            // Allow specific roles only.
-            $current_user_roles = $this->user->get_role( get_current_user_id() );
+		// If access type is not empty. Editor limits the commenting.
+		if ( ! empty ( $access_type ) ) {
+			// Allow administrator.
+			if ( current_user_can( 'manage_option' ) ) {
+				return apply_filters( 'subway_post_discussion_allow_comment', true );
+			}
 
-            $allowed_user_roles = get_post_meta( $post_id, 'subway_post_discussion_roles', true );
+			// Allow specific roles only.
+			$current_user_roles = $this->user->get_role( get_current_user_id() );
 
-            if ( array_intersect( $current_user_roles, (array)$allowed_user_roles ) )
-            {
-                return apply_filters('subway_post_discussion_allow_comment', true);
-            }
+			$allowed_user_roles = get_post_meta( $post_id, 'subway_post_discussion_roles', true );
 
-        }
-        
-        return apply_filters('subway_post_discussion_allow_comment', false);
+			if ( array_intersect( $current_user_roles, (array) $allowed_user_roles ) ) {
+				return apply_filters( 'subway_post_discussion_allow_comment', true );
+			}
 
-    }
+		}
 
-    protected function display()
-    {
-        if ( ! $this->is_current_user_allowed() )
-        {
-            $this->view->render('comment-display', []);
-        }
+		return apply_filters( 'subway_post_discussion_allow_comment', false );
 
-    }
+	}
 
-    protected function restrict()
-    {
-        // Check if coming from submit form.
-        $requested_post_id = filter_input( INPUT_POST, 'comment_post_ID', FILTER_VALIDATE_INT);
+	protected function display() {
+		if ( ! $this->is_current_user_allowed() ) {
+			$this->view->render( 'comment-display', [] );
+		}
 
-        // Handle comment submission.
-        $post_id = get_the_id();
-        // Post is coming from submit comment.
-        if ( ! empty ( $requested_post_id ) ) {
-            $post_id = $requested_post_id;
-        }
- 
-        $current_post = get_post( $post_id );
-        
-        if ( "open" === $current_post->comment_status ) 
-        {
+	}
 
-            if ( current_user_can( 'manage_option' ) ) 
-            {
+	protected function restrict() {
+		// Check if coming from submit form.
+		$requested_post_id = filter_input( INPUT_POST, 'comment_post_ID', FILTER_VALIDATE_INT );
 
-                return true;
-            }
+		// Handle comment submission.
+		$post_id = get_the_id();
+		// Post is coming from submit comment.
+		if ( ! empty ( $requested_post_id ) ) {
+			$post_id = $requested_post_id;
+		}
 
-            if ( $this->is_current_user_allowed()) 
-            {
-                return true;
-            }
+		$current_post = get_post( $post_id );
 
-        }
+		if ( "open" === $current_post->comment_status ) {
 
-        return false;
-    }
+			if ( current_user_can( 'manage_option' ) ) {
 
-    public function hook_comments_open()
-    {
-       return $this->restrict();
-    }
+				return true;
+			}
 
-    public function hook_comment_form_comments_closed()
-    {
-        $this->display();
-    }
+			if ( $this->is_current_user_allowed() ) {
+				return true;
+			}
 
-    public function attach_hooks() 
-    {
-        $this->define_hooks();
-    }
+		}
 
-    private function define_hooks()
-    {
+		return false;
+	}
 
-        add_filter('comments_open', array( $this, 'hook_comments_open'), 10, 2 );
-        add_action('comment_form_comments_closed', array( $this, 'hook_comment_form_comments_closed' ) );
-    }
-    
+	public function hook_comments_open() {
+		return $this->restrict();
+	}
+
+	public function hook_comment_form_comments_closed() {
+		$this->display();
+	}
+
+	public function attach_hooks() {
+		$this->define_hooks();
+	}
+
+	private function define_hooks() {
+
+		add_filter( 'comments_open', array( $this, 'hook_comments_open' ), 10, 2 );
+		add_action( 'comment_form_comments_closed', array( $this, 'hook_comment_form_comments_closed' ) );
+	}
+
 }
