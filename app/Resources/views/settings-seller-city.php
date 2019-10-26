d53ab735b5c8aa7d50d46dad1d27d76ac3ceba1e
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/settings-seller-city.php
+++ app/Resources/views/settings-seller-city.php
@@ -1,10 +1,10 @@
 <?php
 $subway_seller_city = get_option( 'subway_seller_city', '' ); ?>
 <input
-	autocomplete="off"
-	name="subway_seller_city"
-	value="<?php echo esc_attr( $subway_seller_city ); ?>"
-	type="text"
-	class="regular-text"
-	placeholder="<?php esc_html_e( 'Enter the name of the city', 'subway' ); ?>"
+        autocomplete="off"
+        name="subway_seller_city"
+        value="<?php echo esc_attr( $subway_seller_city ); ?>"
+        type="text"
+        class="regular-text"
+        placeholder="<?php esc_html_e( 'Enter the name of the city', 'subway' ); ?>"
 />
