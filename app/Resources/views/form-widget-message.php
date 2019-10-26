e13c8532cbcdab02e181a36d5c9fc30e3711ec26
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/form-widget-message.php
+++ app/Resources/views/form-widget-message.php
@@ -1,11 +1,11 @@
-<?php wp_enqueue_style('subway-general'); ?>
+<?php wp_enqueue_style( 'subway-general' ); ?>
 
 <?php if ( isset( $settings['subway-widget-access-roles-message'] ) ): ?>
 	<?php if ( ! empty ( $settings['subway-widget-access-roles-message'] ) ): ?>
 		<?php echo wp_kses_post( $args['before_widget'] ); ?>
-		<div class="widget-subway-no-access-message">
+        <div class="widget-subway-no-access-message">
 			<?php echo wp_kses_post( $settings['subway-widget-access-roles-message'] ); ?>
-		</div>
+        </div>
 		<?php echo wp_kses_post( $args['after_widget'] ); ?>
 	<?php endif; ?>
 <?php endif; ?>
