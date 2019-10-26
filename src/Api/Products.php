190fcf66561ed8c17181239f2cd5a3ddf2202bb3
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:07 +0800

    new update

--- src/Api/Products.php
+++ src/Api/Products.php
@@ -221,7 +221,6 @@ class Products extends \WP_REST_Controller {
 			[ 'jquery' ] );
 
 
-
 		wp_localize_script( 'subway-admin-js', 'subway_api_settings', array(
 			'root'      => esc_url_raw( rest_url() ),
 			'nonce'     => wp_create_nonce( 'wp_rest' ),
