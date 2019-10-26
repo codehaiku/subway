e0012e0518dfc57f4f5de1ff60fd96880bb6f2d1
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/settings-seller-vat.php
+++ app/Resources/views/settings-seller-vat.php
@@ -1,10 +1,10 @@
 <?php
 $subway_seller_vat = get_option( 'subway_seller_vat', '' ); ?>
 <input
-	autocomplete="off"
-	name="subway_seller_vat"
-	value="<?php echo esc_attr( $subway_seller_vat ); ?>"
-	type="text"
-	class="regular-text"
-	placeholder="<?php esc_html_e( 'Your business VAT number', 'subway' ); ?>"
+        autocomplete="off"
+        name="subway_seller_vat"
+        value="<?php echo esc_attr( $subway_seller_vat ); ?>"
+        type="text"
+        class="regular-text"
+        placeholder="<?php esc_html_e( 'Your business VAT number', 'subway' ); ?>"
 />
