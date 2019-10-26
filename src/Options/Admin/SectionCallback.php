f0d7796caac61224fdf84590e6bdb584f415f101
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:07 +0800

    new update

--- src/Options/Admin/SectionCallback.php
+++ src/Options/Admin/SectionCallback.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace Subway\Options\Admin;
 
-class SectionCallback{
+class SectionCallback {
 
 	public function general() {
 
@@ -10,7 +11,7 @@ class SectionCallback{
 
 	public function seller_profile() {
 		echo '<p class="sw-lead">';
-		esc_html_e('Tell your customers about your business info. This info will appear on emails, invoices, and/or receipts.', 'subway');
+		esc_html_e( 'Tell your customers about your business info. This info will appear on emails, invoices, and/or receipts.', 'subway' );
 		echo '</p>';
 
 		return $this;
@@ -18,36 +19,40 @@ class SectionCallback{
 
 	public function pages() {
 		echo '<p class="sw-lead">';
-		esc_html_e('Select a page for each corresponding membership pages', 'subway');
+		esc_html_e( 'Select a page for each corresponding membership pages', 'subway' );
 		echo '</p>';
+
 		return $this;
 	}
 
 	public function archives() {
 		echo '<p class="sw-lead">';
-		esc_html_e('Choose the accessibility type of your archive pages.', 'subway');
+		esc_html_e( 'Choose the accessibility type of your archive pages.', 'subway' );
 		echo '</p>';
+
 		return $this;
 	}
 
 	public function login_redirect() {
 		echo '<p class="sw-lead">';
-		esc_html_e('Choose the destination page after the user logs in.', 'subway');
+		esc_html_e( 'Choose the destination page after the user logs in.', 'subway' );
 		echo '</p>';
+
 		return $this;
 	}
 
 	public function system_messages() {
 		echo '<p class="sw-lead">';
-		esc_html_e('You can use the options below to
-		 change the default system messages.', 'subway');
+		esc_html_e( 'You can use the options below to
+		 change the default system messages.', 'subway' );
 		echo '</p>';
+
 		return $this;
 	}
 
 	public function paypal() {
 		echo '<p class="sw-lead">';
-		esc_html_e('PayPal Payments Settings.', 'subway');
+		esc_html_e( 'PayPal Payments Settings.', 'subway' );
 		echo '</p>';
 	}
 }
