cc78f95824cb55f373ccdb2e86d342d412d738d3
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/settings-tax-rate.php
+++ app/Resources/views/settings-tax-rate.php
@@ -8,8 +8,8 @@
        value="<?php echo esc_attr( floatval( $tax_rate ) ); ?>"
        max="100" placeholder="0.00" size="4"/> %
 <p class="howto">
-    <?php esc_html_e('Enter the percentage of tax you would like to charge on top of the listing price.', 'subway'); ?>
+	<?php esc_html_e( 'Enter the percentage of tax you would like to charge on top of the listing price.', 'subway' ); ?>
     <strong>
-	    <?php esc_html_e('No need to add %', 'subway'); ?>
+		<?php esc_html_e( 'No need to add %', 'subway' ); ?>
     </strong>
 </p>
