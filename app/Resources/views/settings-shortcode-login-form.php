0c34b7f33079927efb219c578ba524f88c9c4232
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/settings-shortcode-login-form.php
+++ app/Resources/views/settings-shortcode-login-form.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<?php $content = get_option('subway_redirected_message_login_form',
-		esc_html__('Members only page. Please use the login form below to access the page.','subway'));  ; ?>
+<?php $content = get_option( 'subway_redirected_message_login_form',
+	esc_html__( 'Members only page. Please use the login form below to access the page.', 'subway' ) );; ?>
 
-<?php $settings = apply_filters('subway_redirected_message_login_form_editor',
-		array( 'teeny' => true, 'media_buttons' => true, 'editor_height' => 100 )); ?>
+<?php $settings = apply_filters( 'subway_redirected_message_login_form_editor',
+	array( 'teeny' => true, 'media_buttons' => true, 'editor_height' => 100 ) ); ?>
 
 <?php echo wp_editor( $content, 'subway_redirected_message_login_form', $settings ); ?>
 
