57f7fba296464fb5e16c0b9ac6308c9a42ada310
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:05 +0800

    new update

--- app/Resources/views/form-admin-general-settings.php
+++ app/Resources/views/form-admin-general-settings.php
@@ -1,10 +1,10 @@
 <div class="wrap">
-	<h2>
-		<?php esc_html_e('General Settings', 'subway'); ?>
-	</h2>
-	<form id="subway-settings-form" action="options.php" method="POST">
-		<?php settings_fields('subway-settings-group'); ?>
-		<?php do_settings_sections('subway-settings-section'); ?>
+    <h2>
+		<?php esc_html_e( 'General Settings', 'subway' ); ?>
+    </h2>
+    <form id="subway-settings-form" action="options.php" method="POST">
+		<?php settings_fields( 'subway-settings-group' ); ?>
+		<?php do_settings_sections( 'subway-settings-section' ); ?>
 		<?php submit_button(); ?>
-	</form>
+    </form>
 </div>
