a966dae58ccedeec469ca537a8d275cebb99696e
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/settings-tax-display.php
+++ app/Resources/views/settings-tax-display.php
@@ -8,11 +8,11 @@
     <label>
 
         <input
-            <?php checked('1',$subway_display_tax,true); ?>
+			<?php checked( '1', $subway_display_tax, true ); ?>
                 name="subway_display_tax"
                 value="1" type="checkbox"
         />
-	    <?php esc_html_e('Check to include the tax rate on listing price.', 'subway'); ?>
+		<?php esc_html_e( 'Check to include the tax rate on listing price.', 'subway' ); ?>
     </label>
 
 </p>
