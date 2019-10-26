edcb85da9ac63e0632cc2b25f40ddbc84f75ed18
Joseph G <codehaiku.io@gmail.com>
2019-10-26 13:18:06 +0800

    new update

--- app/Resources/views/form-memberships-tools.php
+++ app/Resources/views/form-memberships-tools.php
@@ -2,24 +2,24 @@
 
     <h1 class="wp-heading-inline">
 
-        <?php esc_html_e( 'Tools', 'subway' ); ?></h1>
+		<?php esc_html_e( 'Tools', 'subway' ); ?></h1>
 
 
     <hr class="wp-header-end">
-    <?php $repaired = filter_input(1, 'repaired',516); ?>
+	<?php $repaired = filter_input( 1, 'repaired', 516 ); ?>
 
-    <?php if ( ! empty( $repaired ) ): ?>
-    <div id="message" class="updated notice is-dismissible">
-        <p>
-            <?php esc_html_e('Successfully updated the count record of the selected component.', 'subway'); ?>
-        </p>
-        <button type="button" class="notice-dismiss">
+	<?php if ( ! empty( $repaired ) ): ?>
+        <div id="message" class="updated notice is-dismissible">
+            <p>
+				<?php esc_html_e( 'Successfully updated the count record of the selected component.', 'subway' ); ?>
+            </p>
+            <button type="button" class="notice-dismiss">
             <span class="screen-reader-text">
-                <?php esc_html_e('Dismiss this notice', 'subway'); ?>
+                <?php esc_html_e( 'Dismiss this notice', 'subway' ); ?>
             </span>
-        </button>
-    </div>
-    <?php endif; ?>
+            </button>
+        </div>
+	<?php endif; ?>
     <h3>
 		<?php esc_html_e( 'Repair Counts', 'subway' ); ?>
     </h3>
@@ -31,7 +31,7 @@
 
     <p class="lead">
         <strong>
-		<?php esc_html_e( 'NOTE: This tool runs expensive database query. Please run each repair tool one at a time.', 'subway' ); ?>
+			<?php esc_html_e( 'NOTE: This tool runs expensive database query. Please run each repair tool one at a time.', 'subway' ); ?>
         </strong>
     </p>
 
@@ -39,25 +39,25 @@
         <tr>
             <th>
                 <strong>
-	                <?php esc_html_e('Component', 'subway'); ?>
+					<?php esc_html_e( 'Component', 'subway' ); ?>
                 </strong>
             </th>
             <th>
                 <strong><span class="dashicons dashicons-hammer" style="margin-top: -2.5px;"></span>
-			        <?php esc_html_e('Actions', 'subway'); ?>
+					<?php esc_html_e( 'Actions', 'subway' ); ?>
                 </strong>
             </th>
         </tr>
         <!--.Products-->
         <tr>
             <td>
-                <?php esc_html_e('Membership Plans', 'subway'); ?>
+				<?php esc_html_e( 'Membership Plans', 'subway' ); ?>
             </td>
             <td>
-                <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
-                    <input type="hidden" name="action" value="repair_products_count" />
+                <form method="POST" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+                    <input type="hidden" name="action" value="repair_products_count"/>
                     <button type="submit" class="button button-primary button-large">
-			            <?php esc_html_e('Repair Plan Count', 'subway'); ?>
+						<?php esc_html_e( 'Repair Plan Count', 'subway' ); ?>
                     </button>
                 </form>
             </td>
@@ -66,13 +66,13 @@
         <!--.Products-->
         <tr>
             <td>
-	            <?php esc_html_e('Orders', 'subway'); ?>
+				<?php esc_html_e( 'Orders', 'subway' ); ?>
             </td>
             <td>
-                <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
-                    <input type="hidden" name="action" value="repair_orders_count" />
+                <form method="POST" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+                    <input type="hidden" name="action" value="repair_orders_count"/>
                     <button type="submit" class="button button-primary button-large">
-			            <?php esc_html_e('Repair Orders Count', 'subway'); ?>
+						<?php esc_html_e( 'Repair Orders Count', 'subway' ); ?>
                     </button>
                 </form>
             </td>
