bb68352114938ed71c2e8ca189d2d9bd117c4148
Joseph G <codehaiku.io@gmail.com>
2019-10-28 11:47:01 +0800

    Refactor by moving shortcodes to directory.

--- app/Resources/views/settings-paypal-client-secret.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	return;
-}
-?>
-<input type="text" class="widefat" name="subway_paypal_client_secret"
-       value="<?php echo esc_attr( get_option( 'subway_paypal_client_secret' ) ) ?>"/>
