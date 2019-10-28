976a68337def4f69cc0ddcc7c6fca789088e1e5c
Joseph G <codehaiku.io@gmail.com>
2019-10-28 15:27:40 +0800

    New updates

--- app/Resources/views/shortcodes/product-plans.php
+++ app/Resources/views/shortcodes/product-plans.php
@@ -5,10 +5,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <?php if ( $plans ): ?>
     <form action="<?php echo esc_url( $plan->get_plan_checkout_url() ); ?>" method="GET">
-        <table class="subway-membership-product-plans">
+        <table class="subway-membership-product-plans subway-mg-top-zero subway-mg-bot-zero">
             <thead>
             <tr>
-                <th colspan="2">
+                <th>
 					<?php esc_html_e( 'Select Membership Plan', 'subway' ); ?>
                 </th>
             </tr>
@@ -16,23 +16,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php foreach ( $plans as $plan ): ?>
                 <tr>
                     <td>
-						<?php echo esc_html( $plan->get_displayed_price() ); ?>
-                        /
-						<?php echo esc_html( $plan->get_type() ); ?>
-                    </td>
-                    <td>
 						<?php $id = sprintf( 'membership-plan-%d', $plan->get_id() ); ?>
+
                         <label for="<?php echo esc_attr( $id ); ?>">
                             <input required id="<?php echo esc_attr( $id ); ?>" type="radio" name="plan_id"
                                    value="<?php echo esc_attr( $plan->get_id() ); ?>">
 							<?php echo esc_html( $plan->get_name() ); ?>
+
                         </label>
+                        <em>
+						<?php echo esc_html( $plan->get_displayed_price() ); ?>
+                        /
+						<?php echo esc_html( $plan->get_type() ); ?>
+                        </em>
                     </td>
                 </tr>
 			<?php endforeach; ?>
         </table>
-        <button type="submit" class="sw-button">
-            <?php esc_html_e('Select Membership Plan', 'subway'); ?>
+        <button type="submit" class="sw-button subway-mg-top-zero">
+			<?php esc_html_e( 'Select Membership Plan', 'subway' ); ?>
         </button>
     </form>
 <?php endif; ?>
