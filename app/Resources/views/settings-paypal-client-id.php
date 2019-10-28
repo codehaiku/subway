0731e1c26123655ea14b294c8b57515852faae9c
Joseph G <codehaiku.io@gmail.com>
2019-10-28 11:47:01 +0800

    Refactor by moving shortcodes to directory.

--- app/Resources/views/settings-paypal-client-id.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	return;
-}
-?>
-<input type="text" class="widefat" name="subway_paypal_client_id"
-       value="<?php echo esc_attr( get_option( 'subway_paypal_client_id' ) ) ?>"/>
