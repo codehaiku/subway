2869bf67fd8ea4c76ed147e082146569cf308e74
Joseph G <codehaiku.io@gmail.com>
2019-10-30 01:34:58 +0800

    Added check to see if the current member already has plan

--- app/Resources/views/shortcodes/checkout.php
+++ app/Resources/views/shortcodes/checkout.php
@@ -2,82 +2,100 @@
 
 	<?php $plan_id = $plan->get_id(); ?>
 
-	<?php $user = wp_get_current_user(); ?>
+	<?php $wp_user = wp_get_current_user(); ?>
 
-    <div id="subway-checkout-wrap">
+	<?php $user = new \Subway\User\User() ?>
 
-        <div id="subway-checkout-inner-wrap">
+	<?php $user->set_id( get_current_user_id() ); ?>
 
-            <form class="sw-form" action="" method="POST">
+	<?php if ( $user->has_plan( $plan_id ) ): ?>
+        <div class="subway-alert-danger subway-alert">
+            <p>
+				<?php esc_html_e( 'You have chosen a membership plan which is the same as your current membership plan. Please select a different membership plan to switch.', 'subway' ); ?>
+            </p>
+        </div>
+        <a class="sw-button" href="<?php echo esc_url( $plan->get_plan_url( false ) ); ?>"
+           title="<?php esc_attr_e( 'Select different membership plan' ); ?>">
+			<?php esc_attr_e( 'Select Different Membership Plan' ); ?>
+        </a>
+	<?php else: ?>
 
-                <input type="hidden" name="sw-action" value="checkout"/>
+        <div id="subway-checkout-wrap">
 
-                <input type="hidden" name="sw-product-id" value="<?php echo absint( $plan_id ); ?>"/>
+            <div id="subway-checkout-inner-wrap">
 
-                <div class="subway-checkout-user-info">
+                <form class="sw-form" action="" method="POST">
 
-                    <div class="subway-checkout-user-info-title">
+                    <input type="hidden" name="sw-action" value="checkout"/>
 
-                        <h3>
-							<?php esc_html_e( 'Review Order', 'subway' ); ?>
-                        </h3>
+                    <input type="hidden" name="sw-product-id" value="<?php echo absint( $plan_id ); ?>"/>
 
-                    </div>
+                    <div class="subway-checkout-user-info">
 
-					<?php if ( is_user_logged_in() ): ?>
+                        <div class="subway-checkout-user-info-title">
 
-                    <div class="subway-checkout-user-info-table-wrap">
+                            <h3>
+								<?php esc_html_e( 'Review Order', 'subway' ); ?>
+                            </h3>
 
-						<?php $this->render( 'checkout-personal-information',
+                        </div>
+
+						<?php if ( is_user_logged_in() ): ?>
+
+                        <div class="subway-checkout-user-info-table-wrap">
+
+							<?php $this->render( 'checkout-personal-information',
+								[
+									'user'    => $wp_user,
+									'options' => $options,
+									'plan'    => $plan
+								], false, 'shortcodes' ); ?>
+
+                        </div>
+
+                    </div><!--.subway-checkout-user-info-->
+
+                    <div class="subway-checkout-review-order-wrap">
+
+						<?php
+						$view->render(
+							'checkout-table',
 							[
-								'user'    => $user,
-								'options' => $options,
-								'plan'    => $plan
-							], false, 'shortcodes' ); ?>
+								'plan'     => $plan,
+								'currency' => $currency,
+								'options'  => $options
+							] );
+						?>
 
-                    </div>
+                    </div><!--.subway-checkout-review-order-->
 
-                </div><!--.subway-checkout-user-info-->
+					<?php else: ?>
 
-                <div class="subway-checkout-review-order-wrap">
+                        <div class="subway-alert subway-alert-info">
+                            <p>
+								<?php esc_html_e( 'Please go to memberships products page, select a membership plan, and register.', 'subway' ); ?>
+                                <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>"
+                                   title="<?php esc_attr_e( 'Select Membership Plan', 'subway' ); ?>">
+									<?php esc_html_e( 'Select Membership Plan', 'subway' ); ?>
+                                </a>
+                            </p>
+                        </div>
 
-					<?php
-					$view->render(
-						'checkout-table',
-						[
-							'plan'     => $plan,
-							'currency' => $currency,
-							'options'  => $options
-						] );
-					?>
+					<?php endif; ?>
 
-                </div><!--.subway-checkout-review-order-->
+                </form>
 
-				<?php else: ?>
+            </div><!--.subway-checkout-inner-wrap-->
 
-                    <div class="subway-alert subway-alert-info">
-                        <p>
-							<?php esc_html_e( 'Please go to memberships products page, select a membership plan, and register.', 'subway' ); ?>
-                            <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>"
-                               title="<?php esc_attr_e( 'Select Membership Plan', 'subway' ); ?>">
-								<?php esc_html_e( 'Select Membership Plan', 'subway' ); ?>
-                            </a>
-                        </p>
-                    </div>
+        </div><!--.subway-checkout-wrap-->
 
-				<?php endif; ?>
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+            }
+        </script>
+	<?php endif; ?>
 
-            </form>
-
-        </div><!--.subway-checkout-inner-wrap-->
-
-    </div><!--.subway-checkout-wrap-->
-
-    <script>
-        if (window.history.replaceState) {
-            window.history.replaceState(null, null, window.location.href);
-        }
-    </script>
 
 <?php else: ?>
     <div class="subway-alert subway-alert-info">
