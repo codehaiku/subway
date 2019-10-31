378027cb8e8057f48645baa627461c5fd693b982
Joseph G <codehaiku.io@gmail.com>
2019-10-31 23:34:13 +0800

    Re-layout the checkout page

---
+++ app/Resources/views/checkout-order.php
@@ -0,0 +1,62 @@
+<table class="subway-checkout-review-order">
+    <thead>
+    <tr>
+        <th colspan="2">
+			<?php esc_html_e( 'Selected Memberships Plan', 'subway' ); ?>
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+            <strong>
+                <h3 class="subway-mg-bot-zero subway-mg-top-zero"><?php echo $plan->get_product_link(); ?>
+                </h3>
+                <strong>
+					<?php echo esc_html( $plan->get_name() ); ?>
+                </strong>
+            </strong>
+            <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>"
+               title="<?php esc_attr_e( '(Change)', 'subway' ); ?>">
+				<?php esc_html_e( '(Change)', 'subway' ); ?>
+            </a>
+        </td>
+        <td>
+            <strong>
+				<?php echo esc_html( $plan->get_displayed_price_without_tax() ); ?>
+            </strong>
+        </td>
+    </tr>
+    </tbody>
+
+    <tfoot>
+    <tr>
+        <td>
+			<?php esc_html_e( 'Subtotal', 'subway' ); ?>
+        </td>
+        <td>
+			<?php echo esc_html( $plan->get_displayed_price_without_tax() ); ?>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+			<?php esc_html_e( 'Tax', 'subway' ); ?>
+        </td>
+        <td>
+			<?php echo esc_html( sprintf( '%s%%', $plan->get_tax_rate() ) ); ?>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+			<?php esc_html_e( 'Total', 'subway' ); ?>
+        </td>
+        <td>
+            <strong>
+				<?php echo esc_html( $plan->get_displayed_price() ); ?>
+            </strong>
+        </td>
+    </tr>
+    </tfoot>
+</table>
