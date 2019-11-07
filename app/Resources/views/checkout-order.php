26d09a22daeabd23ecf8b77745f66770da02ea9b
Joseph G <codehaiku.io@gmail.com>
2019-11-07 23:03:57 +0800

    Started working on trials.

--- app/Resources/views/checkout-order.php
+++ app/Resources/views/checkout-order.php
@@ -1,3 +1,11 @@
+<?php $is_trial = filter_input( 1, 'is_trial', 516 ); ?>
+<?php $pricing = $plan->get_pricing(); ?>
+<?php $checkout->set_plan( $plan ); ?>
+
+<?php if ( ! empty( $is_trial ) ): ?>
+	<?php $checkout->set_is_trial( true ); ?>
+<?php endif; ?>
+
 <table class="subway-checkout-review-order">
     <thead>
     <tr>
@@ -9,31 +17,27 @@
     <tbody>
     <tr>
         <td>
-            <strong>
-                <h3 class="subway-mg-bot-zero subway-mg-top-zero"><?php echo $plan->get_product_link(); ?>
-                </h3>
-                <strong>
-					<?php echo esc_html( $plan->get_name() ); ?>
-                </strong>
-            </strong>
-            <a href="<?php echo esc_url( $options->get_membership_page_url() ); ?>"
-               title="<?php esc_attr_e( '(Change)', 'subway' ); ?>">
-				<?php esc_html_e( '(Change)', 'subway' ); ?>
-            </a>
-            <?php $pricing = new \Subway\Memberships\Plan\Pricing\Controller(); ?>
-	        <?php $pricing->set_plan_id( $plan->get_id() ); ?>
-	        <?php $pricing = $pricing->get(); ?>
-	        <?php if ( $pricing ): ?>
-                <span class="product-plan-pricing-text">
-                    <?php echo esc_html( $pricing->get_text( $plan ) ); ?>
-                </span>
-	        <?php endif; ?>
+
+            <h4 class="subway-mg-bot-zero subway-mg-top-zero">
+				<?php echo $checkout->get_plan()->get_product_link(); ?>
+            </h4>
+
+            <h3 class="subway-mg-bot-zero subway-mg-top-zero">
+				<?php echo esc_html( $checkout->get_plan()->get_name() ); ?>
+            </h3>
+
+            <span class="product-plan-pricing-text">
+                <?php echo esc_html( $checkout->get_plan()->get_pricing()->get_text( $plan ) ); ?>
+            </span>
+
         </td>
+
         <td>
             <strong>
-				<?php echo esc_html( $plan->get_displayed_price_without_tax() ); ?>
+				<?php echo esc_html( $checkout->get_price() ); ?>
             </strong>
         </td>
+
     </tr>
     </tbody>
 
@@ -43,18 +47,20 @@
 			<?php esc_html_e( 'Subtotal', 'subway' ); ?>
         </td>
         <td>
-			<?php echo esc_html( $plan->get_displayed_price_without_tax() ); ?>
+			<?php echo esc_html( $checkout->get_subtotal() ); ?>
         </td>
     </tr>
 
-    <tr>
-        <td>
-			<?php esc_html_e( 'Tax', 'subway' ); ?>
-        </td>
-        <td>
-			<?php echo esc_html( sprintf( '%s%%', $plan->get_tax_rate() ) ); ?>
-        </td>
-    </tr>
+	<?php if ( $checkout->get_tax_rate() > 0 ): ?>
+        <tr>
+            <td>
+				<?php esc_html_e( 'Tax', 'subway' ); ?>
+            </td>
+            <td>
+				<?php echo esc_html( sprintf( '%s%%', $checkout->get_tax_rate() ) ); ?>
+            </td>
+        </tr>
+	<?php endif; ?>
 
     <tr>
         <td>
@@ -62,7 +68,7 @@
         </td>
         <td>
             <strong>
-				<?php echo esc_html( $plan->get_taxed_price() ); ?>
+				<?php echo esc_html( $checkout->get_total() ); ?>
             </strong>
         </td>
     </tr>
